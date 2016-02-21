cf033ade9413f68c83ffb0ff2a8a50bf0b385381
sssteven <sssteven@hotmail.co.uk>
2016-02-21 19:47:16 +0000

    first

---
+++ CodeFlow.java
@@ -0,0 +1,39 @@
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CodeFlow {
+
+	public static void main(String args[]) {
+		CodeFlow newCF = new CodeFlow();
+		newCF.launch(2);
+	}
+
+	public void launch(int userInput) {
+		 List<Integer> intList = new ArrayList<Integer>();
+		 intList.add(1);
+		 intList.add(2);
+		 intList.add(3);
+		 intList.add(4);
+		 intList.add(5);
+		 intList.add(6);
+		 try {
+			intList.remove(0);
+			System.out.println(intList.get(userInput));
+			intList.remove(0);
+			System.out.println(intList.get(userInput));
+		 	intList.remove(0);
+			System.out.println(intList.get(userInput));
+			intList.remove(0);
+			System.out.println(intList.get(userInput));
+			intList.remove(0);
+			System.out.println(intList.get(userInput));
+			intList.remove(0);
+			System.out.println(intList.get(userInput));
+			intList.remove(0);
+			System.out.println(intList.get(userInput));
+		} catch (IndexOutOfBoundsException ex) {
+			ex.printStackTrace();
+	 	}  
+	}
+}
